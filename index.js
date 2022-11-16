30a9152b7970fc9ce416fc3fcbdd2fae50a2ae17
annpoddubnaya <pod.dubnaya.anna1990@gmail.com>
2022-11-17 01:29:53 +0300

    add:slider-mob

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//-------burger
 const burgerBtn = document.querySelector('.header');
 const bar1 = document.querySelector('.bar1');
 const bar2 = document.querySelector('.bar2');
@@ -12,3 +14,81 @@ bar1.classList.toggle('open');
 bar2.classList.toggle('open');
 bar3.classList.toggle('open');
 }  );
+
+
+//-------slider
+
+const sliderLine = document.querySelector('.progects__items-slider');
+const btnRight = document.querySelector('.button-right');
+const btnLeft = document.querySelector('.button-left');
+
+let offset = 0;
+
+btnRight.addEventListener('click', function(){
+offset = offset + 440;
+if(offset > 2200){
+    offset = 0;
+}
+sliderLine.style.left = -offset + 'px';
+});
+
+btnLeft.addEventListener('click', function(){
+    offset = offset - 440;
+    if(offset < 0){
+        offset = 2200;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+//---------slider-mob
+
+const slidLine = document.querySelector('.slider-mob__items-line');
+const btnPrev = document.querySelector('.button-prev');
+const btnNext = document.querySelector('.button-next');
+
+let step = 0;
+
+btnPrev.addEventListener('click', function(){
+    step = step + 340;
+    if(step > 1700){
+        step = 0;
+    }
+    slidLine.style.left = -step + 'px';
+});
+
+btnNext.addEventListener('click', function(){
+    step = step - 340;
+    if(step < 0){
+        step = 1700;
+    }
+    slidLine.style.left = -step + 'px';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
